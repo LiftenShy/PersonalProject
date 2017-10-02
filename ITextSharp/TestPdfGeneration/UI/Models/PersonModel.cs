9c1538d8a82010f0f31213fab5c4f24cbf55d834
GOMEL\Siarhei_Privalau <Siarhei_Privalau@epam.com>
2017-10-02 17:12:30 +0300

    Add drawing

---
+++ ITextSharp/TestPdfGeneration/UI/Models/PersonModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPdfGeneration.UI.Models
+{
+    public static class PersonModel
+    {
+        static string person = @"    __(o_o)__  " + '\n' +
+                               @"       (|)      " + '\n' +
+                               @"       / \      " + '\n';
+
+
+        /*Console.WriteLine(
+                                    @"          ( \\ ) " + '\n' +
+                                    @"             \\  " + '\n' +
+                                    @"             /\\ " + '\n' +
+                                    @"    ___(>_<)/    " + '\n' +
+                                    @"        (|)      " + '\n' +
+                                    @"        / \      " + '\n' +
+                                    @"       /   \     " + '\n'); */
+    }
+}
